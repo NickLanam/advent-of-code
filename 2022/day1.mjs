17627cb32b0d817a2dbb2138920fd515300ad099
Nick Lanam <nick.lanam@tekraq.com>
2022-11-30 21:09:16 -0800

    2022 Day 1

--- 2022/day1.mjs
+++ 2022/day1.mjs
@@ -1,10 +1,17 @@
+import { sum } from './utils/array.mjs';
+
 (await import('./aoc.mjs')).default(
   2022, 1,
+  (maxCalories) => maxCalories[0], 24000,
+  (maxCalories) => sum(maxCalories.slice(0, 3)), 45000,
   (data) => {
-    return 'NYI';
-  }, 'NYI',
-  (data) => {
-    return 'NYI';
-  }, 'NYI',
-  data => data
+    let e = [[]];
+    for (const c of data) {
+      if (!c) e.push([]);
+      else e[e.length - 1].push(+c);
+    }
+    e = e.map(v => sum(v));
+    e.sort((a, b) => b - a);
+    return e;
+  }
 );
