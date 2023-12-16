3b99f49b4bb0c0e56ce974cb9ca0a651ebe77a09
Nick Lanam <nick@nicklanam.com>
2023-12-16 12:19:46 -0800

    2023 Day 16 - 415ms total

--- 2023/day16.mjs
+++ 2023/day16.mjs
@@ -28,7 +28,7 @@ const FOLLOW_RES = [
   [[LEFT], [LEFT], [UP, DOWN], [DOWN], [UP]],
 ];
 
-const solve = (charGrid, entryPoint) => {
+const solve = (charGrid, entryPoint, exitPoints) => {
   // Each cell is an array of seen directions out of that cell.
   // It was a Set(), but that's slower than an array with so few elements.
   const grid = charGrid.map((row) => row.map(() => [false, false, false, false]));
@@ -40,14 +40,25 @@ const solve = (charGrid, entryPoint) => {
   const beams = [entryPoint];
   while (beams.length) {
     const [x, y, dir] = beams.pop();
+
     if (x >= 0 && x < charGrid[0].length && y >= 0 && y < charGrid.length) {
       // Storing a number is 300ms faster on part2 than doing `${x},${y}`!
       energizedCoords.add(x * 1_000 + y);
+    } else {
+      // If a beam exits from the grid, then entering a new beam at that
+      // location facing back in would get the same score (the path works
+      // the same way backwards and forwards, even with the splitters).
+      exitPoints.add(x * 1_000 + y);
     }
+
     const fx = x + X_DIR[dir];
     const fy = y + Y_DIR[dir];
     const fc = charGrid[fy]?.[fx];
-    if (fc != null) {
+    if (fc == null) {
+      // Same deal as above - make note of places that a beam exits to not
+      // bother computing paths starting from that same location again.
+      exitPoints.add(fx * 1_000 + y);
+    } else {
       for (const ndir of FOLLOW_RES[dir][fc]) {
         const stored = grid[fy]?.[fx];
         if (stored) {
@@ -64,26 +75,28 @@ const solve = (charGrid, entryPoint) => {
 };
 
 // Start off-screen because 0,0 is a reflector in the real input
-const part1 = (charGrid) => solve(charGrid, [-1, 0, RIGHT]);
+const part1 = (charGrid) => solve(charGrid, [-1, 0, RIGHT], new Set());
 
-// TODO: This still takes on average 300 milliseconds on my input.
-// Would memoizing the energized set per (coord, dir) pair be faster?
+// This takes on average 260 milliseconds on my input.
+// TODO: Is there any further pruning of search space we can do?
+// TODO: Would memoizing energized sets for (coord, dir) be even faster?
 const part2 = (charGrid) => {
   const w = charGrid[0].length;
   const h = charGrid.length;
   let best = -Infinity;
+  const exitPoints = new Set();
   for (let x = 0; x < w; x++) {
     best = Math.max(
       best,
-      solve(charGrid, [x, -1, DOWN]),
-      solve(charGrid, [x, h, UP]),
+      exitPoints.has(x * 1_000 - 1) ? -Infinity : solve(charGrid, [x, -1, DOWN], exitPoints),
+      exitPoints.has(x * 1_000 + h) ? -Infinity : solve(charGrid, [x, h, UP], exitPoints),
     );
   }
   for (let y = 0; y < h; y++) {
     best = Math.max(
       best,
-      solve(charGrid, [-1, y, RIGHT]),
-      solve(charGrid, [w, y, LEFT]),
+      exitPoints.has(-1_000 + y) ? -Infinity : solve(charGrid, [-1, y, RIGHT], exitPoints),
+      exitPoints.has(w * 1_000 + y) ? -Infinity : solve(charGrid, [w, y, LEFT], exitPoints),
     );
   }
   return best;
