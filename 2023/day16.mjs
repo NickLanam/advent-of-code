0134fa14e800548f2e479f3454709e904248dbd1
Nick Lanam <nick@nicklanam.com>
2023-12-16 10:38:40 -0800

    2023 Day 16 - 520ms total

--- 2023/day16.mjs
+++ 2023/day16.mjs
@@ -3,46 +3,38 @@ import aoc from './aoc.mjs';
 const part1expected = 46;
 const part2expected = 51;
 
-const parse = lines => lines.map(l => l.split(''));
+// Const ints instead of one-character strings saves ~50ms on part 2.
+const UP = 0;
+const RIGHT = 1;
+const DOWN = 2;
+const LEFT = 3;
 
-const xDir = new Map([['u', 0], ['r', 1], ['d', 0], ['l', -1]]);
-const yDir = new Map([['u', -1], ['r', 0], ['d', 1], ['l', 0]]);
+const DOT = 0;
+const DASH = 1;
+const PIPE = 2;
+const FSLASH = 3;
+const BSLASH = 4;
 
-const FOLLOW_MAP = new Map([
-  ['u', new Map([
-    ['.', ['u']],
-    ['-', ['l', 'r']],
-    ['|', ['u']],
-    ['/', ['r']],
-    ['\\', ['l']],
-  ])],
-  ['r', new Map([
-    ['.', ['r']],
-    ['-', ['r']],
-    ['|', ['u', 'd']],
-    ['/', ['u']],
-    ['\\', ['d']],
-  ])],
-  ['d', new Map([
-    ['.', ['d']],
-    ['-', ['l', 'r']],
-    ['|', ['d']],
-    ['/', ['l']],
-    ['\\', ['r']],
-  ])],
-  ['l', new Map([
-    ['.', ['l']],
-    ['-', ['l']],
-    ['|', ['u', 'd']],
-    ['/', ['d']],
-    ['\\', ['u']],
-  ])],
-]);
+const CHARS = new Map([['.', DOT], ['-', DASH], ['|', PIPE], ['/', FSLASH], ['\\', BSLASH]]);
+const parse = lines => lines.map(l => l.split('').map(c => CHARS.get(c)));
+
+// These were maps, but as arrays they save ~100ms on part 2.
+const X_DIR = [0, 1, 0, -1];
+const Y_DIR = [-1, 0, 1, 0];
+const FOLLOW_RES = [
+  [[UP], [LEFT, RIGHT], [UP], [RIGHT], [LEFT]],
+  [[RIGHT], [RIGHT], [UP, DOWN], [UP], [DOWN]],
+  [[DOWN], [LEFT, RIGHT], [DOWN], [LEFT], [RIGHT]],
+  [[LEFT], [LEFT], [UP, DOWN], [DOWN], [UP]],
+];
 
 const solve = (charGrid, entryPoint) => {
-  const grid = charGrid.map((row) => row.map(() => new Set()));
+  // Each cell is an array of seen directions out of that cell.
+  // It was a Set(), but that's slower than an array with so few elements.
+  const grid = charGrid.map((row) => row.map(() => []));
 
   // Tracking this separately is faster than counting non-empty sets at the end
+  // Since this can have thousands of elements, it IS faster than an array.
   const energizedCoords = new Set();
 
   const beams = [entryPoint];
@@ -52,15 +44,15 @@ const solve = (charGrid, entryPoint) => {
       // Storing a number is 300ms faster on part2 than doing `${x},${y}`!
       energizedCoords.add(x * 1_000 + y);
     }
-    const fx = x + xDir.get(dir);
-    const fy = y + yDir.get(dir);
+    const fx = x + X_DIR[dir];
+    const fy = y + Y_DIR[dir];
     const fc = charGrid[fy]?.[fx];
-    if (fc) {
-      for (const ndir of FOLLOW_MAP.get(dir).get(fc)) {
-        const stored = grid[fy]?.[fx] ?? null;
+    if (fc != null) {
+      for (const ndir of FOLLOW_RES[dir][fc]) {
+        const stored = grid[fy]?.[fx];
         if (stored) {
-          if (!stored.has(ndir)) {
-            stored.add(ndir);
+          if (!stored.includes(ndir)) {
+            stored.push(ndir);
             beams.push([fx, fy, ndir]);
           }
         }
@@ -72,24 +64,26 @@ const solve = (charGrid, entryPoint) => {
 };
 
 // Start off-screen because 0,0 is a reflector in the real input
-const part1 = (charGrid) => solve(charGrid, [-1, 0, 'r']);
+const part1 = (charGrid) => solve(charGrid, [-1, 0, RIGHT]);
 
-// TODO: This still takes about 550 milliseconds on my input.
+// TODO: This still takes on average 350 milliseconds on my input.
 // Would memoizing the energized set per (coord, dir) pair be faster?
 const part2 = (charGrid) => {
+  const w = charGrid[0].length;
+  const h = charGrid.length;
   let best = -Infinity;
-  for (let x = 0; x < charGrid[0].length; x++) {
+  for (let x = 0; x < w; x++) {
     best = Math.max(
       best,
-      solve(charGrid, [x, -1, 'd']),
-      solve(charGrid, [x, charGrid.length, 'u']),
+      solve(charGrid, [x, -1, DOWN]),
+      solve(charGrid, [x, h, UP]),
     );
   }
-  for (let y = 0; y < charGrid.length; y++) {
+  for (let y = 0; y < h; y++) {
     best = Math.max(
       best,
-      solve(charGrid, [-1, y, 'r']),
-      solve(charGrid, [charGrid[0].length, y, 'l']),
+      solve(charGrid, [-1, y, RIGHT]),
+      solve(charGrid, [w, y, LEFT]),
     );
   }
   return best;
