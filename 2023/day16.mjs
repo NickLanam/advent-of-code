a65c3cb2e50dbdd212931212d0c710151d96a01c
Nick Lanam <nick@nicklanam.com>
2023-12-16 09:36:48 -0800

    2023 Day 16 - 0.9 seconds

--- 2023/day16.mjs
+++ 2023/day16.mjs
@@ -7,92 +7,73 @@ const parse = lines => lines.map(l => l.split(''));
 
 const xDir = new Map([['u', 0], ['r', 1], ['d', 0], ['l', -1]]);
 const yDir = new Map([['u', -1], ['r', 0], ['d', 1], ['l', 0]]);
-const followBeam = ({ x, y, dir }, grid) => {
-  const out = [];
-  const dx = xDir.get(dir);
-  const dy = yDir.get(dir);
 
-  const char = grid[y + dy]?.[x + dx] ?? null;
-  switch (char) {
-    case null:
-      // Beam just dissipates if it hits a wall
-      break;
-    case '.':
-      // Beam passes through if it reaches a dot
-      out.push({ x: x + dx, y: y + dy, dir });
-      break;
-    case '-':
-      if (dx === 0) {
-        // Spit out of both pointy ends
-        out.push({ x: x + dx, y: y + dy, dir: 'l' });
-        out.push({ x: x + dx, y: y + dy, dir: 'r' });
-      } else {
-        // Pass right through
-        out.push({ x: x + dx, y: y + dy, dir });
-      }
-      break;
-    case '|':
-      if (dy === 0) {
-        // Spit out of both pointy ends
-        out.push({ x: x + dx, y: y + dy, dir: 'u' });
-        out.push({ x: x + dx, y: y + dy, dir: 'd' });
-      } else {
-        // Pass right through
-        out.push({ x: x + dx, y: y + dy, dir });
-      }
-      break;
-    case '/':
-      if (dx === -1) {
-        out.push({x: x + dx, y: y + dy, dir: 'd' });
-      } else if (dx === 1) {
-        out.push({x: x + dx, y: y + dy, dir: 'u' });
-      } else if (dy === -1) {
-        out.push({x: x + dx, y: y + dy, dir: 'r' });
-      } else if (dy === 1) {
-        out.push({x: x + dx, y: y + dy, dir: 'l' });
-      } else {
-        throw new Error('Bad fallthrough logic on char /');
-      }
-      break;
-    case '\\':
-      if (dx === -1) {
-        out.push({x: x + dx, y: y + dy, dir: 'u' });
-      } else if (dx === 1) {
-        out.push({x: x + dx, y: y + dy, dir: 'd' });
-      } else if (dy === -1) {
-        out.push({x: x + dx, y: y + dy, dir: 'l' });
-      } else if (dy === 1) {
-        out.push({x: x + dx, y: y + dy, dir: 'r' });
-      } else {
-        throw new Error('Bad fallthrough logic on char \\');
-      }
-      break;
-    default:
-      throw new Error('Missed case: ' + char);
-  }
-  return out.filter(({x, y}) => x >= 0 && x < grid[0].length && y >= 0 && y < grid.length);
+const FOLLOW_MAP = new Map([
+  ['u', new Map([
+    ['.', ['u']],
+    ['-', ['l', 'r']],
+    ['|', ['u']],
+    ['/', ['r']],
+    ['\\', ['l']],
+  ])],
+  ['r', new Map([
+    ['.', ['r']],
+    ['-', ['r']],
+    ['|', ['u', 'd']],
+    ['/', ['u']],
+    ['\\', ['d']],
+  ])],
+  ['d', new Map([
+    ['.', ['d']],
+    ['-', ['l', 'r']],
+    ['|', ['d']],
+    ['/', ['l']],
+    ['\\', ['r']],
+  ])],
+  ['l', new Map([
+    ['.', ['l']],
+    ['-', ['l']],
+    ['|', ['u', 'd']],
+    ['/', ['d']],
+    ['\\', ['u']],
+  ])],
+]);
+
+const followBeam = ([px, py, odir], grid) => {
+  const x = px + xDir.get(odir);
+  const y = py + yDir.get(odir);
+  
+  const char = grid[y]?.[x];
+  if (!char) return [];
+
+  const outDirs = FOLLOW_MAP.get(odir).get(char);
+  return outDirs.map((dir) => ([x, y, dir]));
 };
 
 const solve = (charGrid, entryPoint) => {
-  const grid = charGrid.map((row, y) => row.map((char, x) => ({ x, y, char, dirs: new Set() })));
-  const energizedCoords = new Set(); // To simplify answering later
+  const grid = charGrid.map((row) => row.map(() => new Set()));
+
+  // Tracking this separately cost less than counting non-empty sets at the end
+  const energizedCoords = new Set();
 
   let beams = [entryPoint];
   while (true) {
     let hasChanges = false;
     const newBeams = [];
     for (const beam of beams) {
-      if (beam.x >= 0 && beam.x < charGrid[0].length && beam.y >= 0 && beam.y < charGrid.length) {
-        energizedCoords.add(`${beam.x},${beam.y}`);
+      const [x, y] = beam;
+      if (x >= 0 && x < charGrid[0].length && y >= 0 && y < charGrid.length) {
+        // Storing a number is 300ms faster on part2 than doing `${x},${y}`!
+        energizedCoords.add(x * 10_000 + y);
       }
       const followed = followBeam(beam, charGrid);
       for (const f of followed) {
-        const { x, y, dir } = f;
-        const inMap = grid[y]?.[x] ?? null;
+        // const [x, y, dir] = f; // Destructuring costs 100ms on part2!
+        const inMap = grid[f[1]]?.[f[0]] ?? null;
         if (inMap) {
-          if (!inMap.dirs.has(dir)) {
+          if (!inMap.has(f[2])) {
             hasChanges = true;
-            inMap.dirs.add(dir);
+            inMap.add(f[2]);
             newBeams.push(f);
           }
         }
@@ -106,24 +87,26 @@ const solve = (charGrid, entryPoint) => {
 };
 
 // Start off-screen because 0,0 is a reflector in the real input
-const part1 = (grid) => solve(grid, { x: -1, y: 0, dir: 'r' });
+const part1 = (charGrid) => solve(charGrid, [-1, 0, 'r']);
 
-// TODO: This eats up over a second of runtime, by brute forcing all options.
-// There is likely a clever optimization to get under 100ms.
-const part2 = (grid) => {
+// TODO: This still takes nearly a second on my input.
+// I can make solve() even faster, but can I avoid trying every entry point?
+// Could memoize the energized set of every (coordinate, beam) pair
+// to avoid recalculating across or within solves, but that may be slower?
+const part2 = (charGrid) => {
   let best = -Infinity;
-  for (let x = 0; x < grid[0].length; x++) {
+  for (let x = 0; x < charGrid[0].length; x++) {
     best = Math.max(
       best,
-      solve(grid, { x, y: -1, dir: 'd' }),
-      solve(grid, { x, y: grid.length, dir: 'u' }),
+      solve(charGrid, [x, -1, 'd']),
+      solve(charGrid, [x, charGrid.length, 'u']),
     );
   }
-  for (let y = 0; y < grid.length; y++) {
+  for (let y = 0; y < charGrid.length; y++) {
     best = Math.max(
       best,
-      solve(grid, { x: -1, y, dir: 'r' }),
-      solve(grid, { x: grid[0].length, y, dir: 'l' }),
+      solve(charGrid, [-1, y, 'r']),
+      solve(charGrid, [charGrid[0].length, y, 'l']),
     );
   }
   return best;
