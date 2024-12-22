abf0b6124df75c7e4ef25bb85a01ff4cca1d29f3
Nick Lanam <nick@nicklanam.com>
2024-12-21 22:02:20 -0800

    2024 Day 22 - Part 2 (Nine Minute Runtime version)

--- 2024/day22.mjs
+++ 2024/day22.mjs
@@ -42,18 +42,44 @@ const part1 = (secrets) => {
  * @returns {Part2Solution}
  */
 const part2 = (secrets) => {
-  return Number(secrets.map(secret => {
+  const computed = secrets.map(secret => {
     let s = secret;
     let lastDigit = n => +String(Number(n)).at(-1);
     let prev = [lastDigit(s)];
+    const diffs = [];
     for (let i = 0; i < 2_000; i++) {
       s = ((s << 6n) ^ s) % 16777216n;
       s = ((s >> 5n) ^ s) % 16777216n;
       s = ((s << 11n) ^ s) % 16777216n;
       prev.push(lastDigit(s));
+      diffs.push(prev[i + 1] - prev[i]);
     }
-    return s;
-  }).reduce((a, c) => a + c, 0n));
+    return { prev, diffs };
+  });
+
+  // Incredibly naive and painfully slow: try every possible set of diffs.
+  // See what score we get.
+  // Keep the largest one.
+  let mostBananas = 0;
+  for (let d0 = -9; d0 <= 9; d0++) {
+    for (let d1 = -9; d1 <= 9; d1++) {
+      for (let d2 = -9; d2 <= 9; d2++) {
+        for (let d3 = -9; d3 <= 9; d3++) {
+          // The scoring method that we should not be using 19**4 times, but hey if it works...
+          const bananas = computed.reduce((a, { prev, diffs }) => {
+            for (let i = 4; i < diffs.length; i++) {
+              if (diffs[i - 3] === d0 && diffs[i - 2] === d1 && diffs[i - 1] === d2 && diffs[i] === d3) {
+                return a + prev[i + 1];
+              }
+            }
+            return a;
+          }, 0);
+          if (bananas > mostBananas) mostBananas = bananas;
+        }
+      }
+    }
+  }
+  return mostBananas;
 };
 
 aoc({
