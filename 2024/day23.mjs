883f85b3857604daa6f658c73ab6da1846b1b66a
Nick Lanam <nick@nicklanam.com>
2024-12-22 22:44:40 -0800

    2024 Day 23 - Part 2, 9sec total due to slow cache

--- 2024/day23.mjs
+++ 2024/day23.mjs
@@ -16,17 +16,12 @@ const part2expected = 'co,de,ka,ta';
  * @returns {ParsedInput}
  */
 const parse = (lines, forPart) => {
-  return lines.map(l => l.split('-'));
-};
-
-/**
- * 
- * @param {ParsedInput} parsed 
- * @returns {Part1Solution}
- */
-const part1 = (parsed) => {
+  const parsed = lines.map(l => l.split('-'));
+  let allNodes = new Set();
   const directConnections = new Map();
   for (const [l, r] of parsed) {
+    allNodes.add(l);
+    allNodes.add(r);
     const el = directConnections.get(l) ?? new Set();
     el.add(r);
     directConnections.set(l, el);
@@ -35,7 +30,17 @@ const part1 = (parsed) => {
     directConnections.set(r, er);
   }
 
-  const allNodes = [...new Set(parsed.flat())];
+  allNodes = [...allNodes];
+  allNodes.sort();
+  return { directConnections, allNodes };
+};
+
+/**
+ * 
+ * @param {ParsedInput} parsed 
+ * @returns {Part1Solution}
+ */
+const part1 = ({ directConnections, allNodes }) => {
   const threes = new Set();
   for (let i = 0; i < allNodes.length - 2; i++) {
     for (let j = i + 1; j < allNodes.length - 1; j++) {
@@ -55,33 +60,50 @@ const part1 = (parsed) => {
   return threes.size;
 };
 
+function findGroupsOfSize(directConnections, allNodes, size, include = new Set(), offset = 0) {
+  const out = [];
+  if (size <= 0) return [];
+
+  for (let i = offset + 1; i < allNodes.length - size + 1; i++) {
+    const el = allNodes[i];
+    if ([...include].every(n => directConnections.get(el).has(n))) {
+      if (size === 1) out.push([el]);
+      else {
+        const sub = findGroupsOfSize(directConnections, allNodes, size - 1, new Set([...include, el]), i);
+        for (const s of sub) {
+          if (s && s.length === size - 1) {
+            out.push([el, ...s]);
+          }
+        }
+      }
+    }
+  }
+  return out;
+}
+
 /**
  * 
  * @param {ParsedInput} parsed 
  * @returns {Part2Solution}
  */
-const part2 = (parsed) => {
-  const directConnections = new Map();
-  for (const [l, r] of parsed) {
-    const el = directConnections.get(l) ?? new Set();
-    el.add(r);
-    directConnections.set(l, el);
-    const er = directConnections.get(r) ?? new Set();
-    er.add(l);
-    directConnections.set(r, er);
-  }
-
-  const allNodes = [...new Set(parsed.flat())];
-  allNodes.sort();
-
+const part2 = ({ directConnections, allNodes }) => {
   // Binary search through the possible group sizes.
   // The highest one we end up finding is the one we should return.
   let lo = 0;
   let hi = allNodes.length;
   let best = [];
-  // TODO: Have to make my part 1 into a dynamic programming solution that takes the target size.
-  //  With that, the binary search here will be easy.
-  //  Making that is the hard part though.
+  while (hi - lo > 1) {
+    const mid = Math.floor((lo + hi) / 2);
+    const groups = findGroupsOfSize(directConnections, allNodes, mid);
+    if (groups.length > 0) {
+      lo = mid;
+      best = groups;
+    } else {
+      hi = mid;
+    }
+  }
+
+  best = best[0];
   best.sort();
   return best.join(',');
 };
