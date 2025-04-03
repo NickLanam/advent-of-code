c489457ce3880011f6ae6cfd1711286303459e7d
Nick Lanam <nick@nicklanam.com>
2025-04-02 17:53:27 -0700

    2017 Day 7 - 50% Speedup with RefCells

--- 2017/src/bin/day07.rs
+++ 2017/src/bin/day07.rs
@@ -1,20 +1,19 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, rc::Rc};
 
 use advent_lib::runner::{Day, PartId};
 use anyhow::{Context, Result, bail};
 use fnv::{FnvBuildHasher, FnvHashMap, FnvHashSet};
 
-#[derive(Debug, Clone)]
 struct Node {
   id: String,
   node_weight: i32,
   total_weight: i32,
-  children: Vec<Node>,
+  children: Vec<Rc<Node>>,
 }
 
 type P1Out = String;
 type P2Out = i32;
-type Parsed = Node;
+type Parsed = Rc<Node>;
 
 struct Solver {}
 impl Day<Parsed, P1Out, P2Out> for Solver {
@@ -23,6 +22,8 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     let mut all_children: FnvHashSet<&str> =
       FnvHashSet::with_capacity_and_hasher(lines.len() - 1, FnvBuildHasher::default());
     let mut explore: VecDeque<&str> = VecDeque::with_capacity(lines.len());
+
+    // To build the graph, we first must know the initial weights and edges
     let mut initial_nodes: FnvHashMap<&str, (i32, Vec<&str>)> =
       FnvHashMap::with_capacity_and_hasher(lines.len(), FnvBuildHasher::default());
 
@@ -47,35 +48,47 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
       .find(|&id| !all_children.contains(id))
       .context("")?;
 
-    let mut hydrated_nodes =
-      FnvHashMap::<&str, Node>::with_capacity_and_hasher(lines.len(), FnvBuildHasher::default());
+    // Storing RefCounts in the backing HashMap allows returning references
+    // to the caller, as well as avoiding any cloning of actual nodes, and in
+    // theory lets the tree have cycles or nodes with multiple parents (though
+    // this puzzle doesn't do that - it's a directed acyclic graph).
+    let mut hydrated_nodes = FnvHashMap::<&str, Rc<Node>>::with_capacity_and_hasher(
+      lines.len(),
+      FnvBuildHasher::default(),
+    );
 
+    // Using a deque, checks elements at the front of the line for
+    // whether all of their children have already been processed.
+    // Return to the back of the line if not.
+    // After one full pass, all leaf nodes are processed and removed
+    // from the deque this way, then further passes walk their way
+    // back towards the root.
     'outer: while !explore.is_empty() {
       let top = explore.pop_front().context("")?;
       let (own_weight, child_names) = initial_nodes.get(top).context("")?;
-      let mut children: Vec<Node> = vec![];
+      let mut children: Vec<Rc<Node>> = vec![];
       let mut child_sum = 0;
       for &child_key in child_names {
         if let Some(child) = hydrated_nodes.get(child_key) {
           child_sum += child.total_weight;
+          // Only cloning the RefCount, much cheaper than cloning the node!
           children.push(child.clone());
         } else {
           explore.push_back(top);
           continue 'outer;
         }
       }
-      hydrated_nodes.insert(
-        top,
-        Node {
-          id: top.to_string(),
-          node_weight: *own_weight,
-          total_weight: *own_weight + child_sum,
-          children,
-        },
-      );
+      let new_node = Rc::new(Node {
+        id: top.to_string(),
+        node_weight: *own_weight,
+        total_weight: *own_weight + child_sum,
+        children,
+      });
+      hydrated_nodes.insert(top, new_node);
     }
 
-    // Now build the tree from that
+    // Finally, return the root node
+    // The clone is on an Rc<Node>, so it's cheap!
     Ok(hydrated_nodes.get(root_id).context("")?.clone())
   }
 
@@ -91,7 +104,7 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
         node_weight,
         children,
         ..
-      } = cursor;
+      } = &**cursor;
 
       // Check how many unique weights this level has (it'll be either 0, 1, or 2).
       let mut weight_incidence = FnvHashMap::<i32, u8>::with_hasher(FnvBuildHasher::default());
