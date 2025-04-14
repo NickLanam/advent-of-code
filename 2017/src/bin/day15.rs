9ab9be2b3892eb3041db30b26536781da1ab580d
Nick Lanam <nick@nicklanam.com>
2025-04-14 10:43:51 -0700

    2017 Day 15 - Faster, but still slow

--- 2017/src/bin/day15.rs
+++ 2017/src/bin/day15.rs
@@ -5,9 +5,25 @@ type P1Out = u64;
 type P2Out = u64;
 type Parsed = (u64, u64);
 
-const MUL_A: u64 = 16_807;
-const MUL_B: u64 = 48_271;
-const DIVISOR: u64 = 2_147_483_647; // 2**31 - 1
+// Turns out, this is a famous pseudo-random number generator:
+// https://en.wikipedia.org/wiki/Lehmer_random_number_generator
+
+// Unfortunately, since it's a PRNG by design, there's no shortcut
+// to compute the answers without actually generating the numbers.
+
+// These two values are common choices of factor for Lehmer RNG:
+const FACTOR_A: u64 = 16_807;
+const FACTOR_B: u64 = 48_271;
+const DIVISOR: u64 = 0x7F_FF_FF_FF; // 2**31 - 1
+
+// Behavior directly from the Wiki article to avoid a division.
+#[inline(always)]
+fn step(seed: &mut u64, factor: u64) {
+  *seed *= factor;
+  while *seed >= DIVISOR {
+    *seed = (*seed & DIVISOR) + (*seed >> 31);
+  }
+}
 
 struct Solver {}
 impl Day<Parsed, P1Out, P2Out> for Solver {
@@ -25,11 +41,9 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
   fn part1(&self, &(seed_a, seed_b): &Parsed, _sample_name: Option<String>) -> Result<P1Out> {
     let (mut a, mut b) = (seed_a, seed_b);
     let mut sum = 0;
-    // TODO: There's something fishy with the input values.
-    // Likely we don't need to iterate at all.
     for _ in 0..40_000_000 {
-      a = (a * MUL_A) % DIVISOR;
-      b = (b * MUL_B) % DIVISOR;
+      step(&mut a, FACTOR_A);
+      step(&mut b, FACTOR_B);
       if (a & 0xFFFF) == (b & 0xFFFF) {
         sum += 1;
       }
@@ -40,16 +54,14 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
   fn part2(&self, &(seed_a, seed_b): &Parsed, _sample_name: Option<String>) -> Result<P2Out> {
     let (mut a, mut b) = (seed_a, seed_b);
     let mut sum = 0;
-    // TODO: There's something fishy with the input values.
-    // Likely we don't need to iterate at all.
     for _ in 0..5_000_000 {
-      a = (a * MUL_A) % DIVISOR;
+      step(&mut a, FACTOR_A);
       while a & 0b11 != 0 {
-        a = (a * MUL_A) % DIVISOR;
+        step(&mut a, FACTOR_A);
       }
-      b = (b * MUL_B) % DIVISOR;
+      step(&mut b, FACTOR_B);
       while b & 0b111 != 0 {
-        b = (b * MUL_B) % DIVISOR;
+        step(&mut b, FACTOR_B);
       }
       if (a & 0xFFFF) == (b & 0xFFFF) {
         sum += 1;
