21c4165a2d4d760eb7ef735427eeb83d1cb42689
Nick Lanam <nick@nicklanam.com>
2025-05-23 20:39:53 -0700

    2018 Day 20 - Tried a shortcut, didn't work

--- 2018/src/bin/day20.rs
+++ 2018/src/bin/day20.rs
@@ -1,5 +1,5 @@
 use advent_lib::runner::{Day, PartId};
-use anyhow::Result;
+use anyhow::{Result, bail};
 
 #[derive(Debug, Default, PartialEq, Hash)]
 struct Room {
@@ -11,13 +11,7 @@ struct Room {
 
 type P1Out = usize;
 type P2Out = usize;
-type Parsed = Vec<char>;
-
-/*
-fn buildMap(chars: &[char]) -> Infinite2dGrid<Room> {
-  //
-}
-*/
+type Parsed = String;
 
 struct Solver {}
 impl Day<Parsed, P1Out, P2Out> for Solver {
@@ -27,24 +21,50 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     _sample_name: Option<String>,
     _for_part: PartId,
   ) -> Result<Parsed> {
-    Ok(lines[0].chars().skip(1).take(lines[0].len() - 2).collect())
+    Ok(lines[0].to_string())
   }
 
-  fn part1(&self, chars: &Parsed, _sample_name: Option<String>) -> Result<P1Out> {
-    /* Actually, Imma try the dumb thing first. * /
-    let map = buildMap(chars);
-    let mut max_len = 0;
-    // TODO: BFS until we find all paths to get the longest distance possible.
-    //  This is a separate phase because of the "each room will be seen AT LEAST ONCE" rule.
-    //  If we were sure to only visit each room once, we wouldn't need to build the map -
-    //  only gives us one way to get it done that may have loops and inefficient routes.
-    Ok(max_len)
-    */
-
-    // Okay, the faster and simpler method IF IT WORKS THAT WAY is to just
-    // replace every nested group with its longest subgroup and count
-    // the resulting string's length. Only if the "at least once" thing isn't
-    // the trap I suspect it is, anyway.
+  fn part1(&self, line: &Parsed, _sample_name: Option<String>) -> Result<P1Out> {
+    // TODO: This first attempt assumed there'd be no loops. It could be adjusted to
+    // remove easily-detected (shorter) loops, but there are sample inputs (and my real one)
+    // that have harder-to-detect ones.
+    // Best to actually build the map and pathfind it.
+    let mut stack: Vec<Vec<usize>> = vec![vec![]];
+    for ch in line.chars().skip(1).take(line.len() - 2) {
+      match ch {
+        'N' | 'E' | 'S' | 'W' => {
+          if let Some(top_group) = stack.last_mut() {
+            if let Some(top_size) = top_group.last_mut() {
+              *top_size += 1;
+            } else {
+              top_group.push(1);
+            }
+          } else {
+            stack.push(vec![1]);
+          }
+        }
+        '(' => {
+          stack.push(vec![0]);
+        }
+        ')' => {
+          let max = *stack.pop().unwrap().iter().max().unwrap();
+          let l = stack.len();
+          *stack[l - 1].last_mut().unwrap() += max;
+        }
+        '|' => {
+          let l = stack.len();
+          stack[l - 1].push(0);
+        }
+        _ => {
+          bail!("Unrecognized char: {ch}");
+        }
+      }
+      // println!("After seeing '{ch}', stack is now:\n  {stack:?}");
+    }
+    // 4221 is too high. Because of the loops.
+    // Can't just replace them either: sure, I can turn 'EW', 'WE', 'NESW', and every other
+    // combination into the empty string, but for example NNNNESSSSW is a loop too.
+    Ok(stack.pop().unwrap().pop().unwrap())
   }
 
   fn part2(&self, _lines: &Parsed, _sample_name: Option<String>) -> Result<P2Out> {
