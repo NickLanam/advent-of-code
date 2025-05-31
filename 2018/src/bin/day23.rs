47081a1e5c3f1593aca919cbac3d89b1ff694c0c
Nick Lanam <nick@nicklanam.com>
2025-05-31 08:33:34 -0700

    2018 Day 23 Part 2 - Different approach works

--- 2018/src/bin/day23.rs
+++ 2018/src/bin/day23.rs
@@ -2,17 +2,12 @@ use advent_lib::runner::{Day, PartId};
 use anyhow::{Context, Result};
 
 type P1Out = usize;
-type P2Out = i64;
+type P2Out = usize;
 type Parsed = Vec<(i64, i64, i64, i64)>;
 
-struct Solver {}
+struct Solver;
 impl Day<Parsed, P1Out, P2Out> for Solver {
-  fn parse(
-    &self,
-    lines: Vec<String>,
-    _sample_name: Option<String>,
-    _for_part: PartId,
-  ) -> Result<Parsed> {
+  fn parse(&self, lines: Vec<String>, _: Option<String>, _: PartId) -> Result<Parsed> {
     let mut out = Vec::with_capacity(lines.len());
     for line in lines {
       let (left, r) = line[5..].split_once(">, r=").context("Bad line")?;
@@ -23,7 +18,8 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     Ok(out)
   }
 
-  fn part1(&self, bots: &Parsed, _sample_name: Option<String>) -> Result<P1Out> {
+  // Trivial implementation of the spec. Takes 2µs.
+  fn part1(&self, bots: &Parsed, _: Option<String>) -> Result<P1Out> {
     let &(lx, ly, lz, lr) = bots.iter().max_by_key(|&&(_, _, _, r)| r).unwrap();
     let mut in_range: usize = 0;
     for &(x, y, z, _r) in bots.iter() {
@@ -35,40 +31,72 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     Ok(in_range)
   }
 
-  fn part2(&self, bots: &Parsed, sample_name: Option<String>) -> Result<P2Out> {
-    // Massive assumption: we can reduce the solution from three dimensions to
-    // one, because the structure of the input and the use of manhattan distance
-    // happens to make it work out such that we can just walk through a sorted
-    // sequence of
-    println!("\nRUN: {sample_name:?}");
+  // Originally, I oversimplified this solution to use a single axis
+  // number line (about 100µs). This got an answer just 22 away from
+  // the correct one, which meant that assumption isn't quite right.
+  // This approach does a dividing search of a bounding box that has
+  // all nodes, similar to how octrees work. It takes 15ms. Sadness.
+  fn part2(&self, bots: &Parsed, _: Option<String>) -> Result<P2Out> {
+    let (mut x_min, mut x_max, mut y_min, mut y_max, mut z_min, mut z_max) =
+      (i64::MAX, i64::MIN, i64::MAX, i64::MIN, i64::MAX, i64::MIN);
+    for &(x, y, z, _r) in bots.iter() {
+      x_min = x_min.min(x);
+      x_max = x_max.max(x);
+      y_min = y_min.min(y);
+      y_max = y_max.max(y);
+      z_min = z_min.min(z);
+      z_max = z_max.max(z);
+    }
 
-    let mut segments: Vec<(i64, i64)> = vec![];
-    for &(x, y, z, r) in bots {
-      let d = x.abs() + y.abs() + z.abs();
-      segments.push((0.max(d - r), 1));
-      segments.push((d + r + 1, -1));
+    let mut range = 1;
+    while range < x_max - x_min {
+      range *= 2;
     }
-    segments.sort_by(|a, b| a.0.cmp(&b.0).then(a.1.cmp(&b.1)));
-    let mut count = 0;
-    let mut max_count = 0;
-    let mut result = 0;
-    for (d, e) in segments {
-      println!("Popped {d}, {e}");
-      count += e;
-      if count > max_count {
-        result = d;
-        max_count = count;
-        println!("  result={result}, max_count={max_count}, count={count}");
+
+    loop {
+      let ru = range as usize;
+      let mut best_in_range = 0;
+      let mut best_dist = 0;
+      let mut best_pos = (0, 0, 0);
+
+      // Divide the current search box into smaller boxes with `range`-long
+      // sides, and check each corner to see how many bots are in its range.
+      // The corner that's seen by the most bots (closest to origin in a tie)
+      // becomes the new center point, and the search range is halved, until
+      // we've refined to a single point. That point's distance from origin
+      // is the answer.
+      for x in (x_min..=x_max).step_by(ru) {
+        for y in (y_min..=y_max).step_by(ru) {
+          for z in (z_min..=z_max).step_by(ru) {
+            let mut in_range = 0;
+            for &(bx, by, bz, br) in bots.iter() {
+              if ((bx.abs_diff(x) + by.abs_diff(y) + bz.abs_diff(z)) as i64 - br) / range <= 0 {
+                in_range += 1;
+              }
+            }
+            let dist = x.abs() + y.abs() + z.abs();
+            if in_range > best_in_range || (in_range == best_in_range && dist < best_dist) {
+              best_in_range = in_range;
+              best_dist = dist;
+              best_pos = (x, y, z);
+            }
+          }
+        }
       }
+
+      if range == 1 {
+        return Ok(best_dist as usize);
+      }
+
+      // Next search is centered on the best point so far, at half radius.
+      x_min = best_pos.0 - range;
+      x_max = best_pos.0 + range;
+      y_min = best_pos.1 - range;
+      y_max = best_pos.1 + range;
+      z_min = best_pos.2 - range;
+      z_max = best_pos.2 + range;
+      range /= 2;
     }
-    // 84_087_794 is too low, so that isn't it...
-    // 84_087_817 is too high, which was the next node that was popped after the too low answer
-    // Meaning the correct answer is in a range of 23 values. I get the correct
-    // answer on the samples, so there's something slightly off with my input
-    // that makes the trick _almost_ but not quite work...
-    // 84_087_805 is "not right" (the midpoint), was hoping I'd get one more chance there.
-    println!("FINAL  result={result}, max_count={max_count}, count={count}");
-    Ok(result)
   }
 }
 
