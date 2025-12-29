d9dfd3aa9655ecd2272182d452da74ceec6874fb
Nick Lanam <nick@nicklanam.com>
2025-12-28 18:41:53 -0800

    2019 Day 17 Part 2

--- 2019/src/bin/day17.rs
+++ 2019/src/bin/day17.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use advent_lib::{
   direction::{CardinalDirection, Rotation},
   grid::Infinite2dSet,
@@ -10,6 +12,7 @@ type P1Out = usize;
 type P2Out = i64;
 type Parsed = Vec<i64>;
 
+/// Exists only to make build_map easier to read
 #[derive(PartialEq)]
 enum Tile {
   Empty,
@@ -43,6 +46,9 @@ struct Map {
   bot_y: i32,
   bot_dir: CardinalDirection,
 }
+/// Trivial parsing exercise: run the program, it outputs a grid of . and #,
+/// turn that into an Infinite2dSet like we do with puzzles where that's the
+/// original input. Just a slightly roundabout way to get there.
 fn build_map(instructions: &[i64]) -> Result<Map> {
   let result = execute(instructions, &[], None, None)?;
   let mut scaffolds = Infinite2dSet::new(1_000);
@@ -95,47 +101,10 @@ fn build_map(instructions: &[i64]) -> Result<Map> {
   })
 }
 
-fn print_grid(map: &Map) {
-  let mut min_x = 0;
-  let mut max_x = 0;
-  let mut min_y = 0;
-  let mut max_y = 0;
-
-  for (x, y) in map.scaffolds.keys() {
-    min_x = min_x.min(x);
-    max_x = max_x.max(x);
-    min_y = min_y.min(y);
-    max_y = max_y.max(y);
-  }
-
-  for y in min_y..=max_y {
-    for x in min_x..=max_x {
-      if x == map.bot_x && y == map.bot_y {
-        match map.bot_dir {
-          CardinalDirection::N => print!("^"),
-          CardinalDirection::E => print!(">"),
-          CardinalDirection::S => print!("v"),
-          CardinalDirection::W => print!("<"),
-        }
-      } else {
-        print!(
-          "{}",
-          if map.scaffolds.contains(x, y) {
-            "#"
-          } else {
-            " "
-          }
-        );
-      }
-    }
-    println!();
-  }
-}
-
 /// The real input looks like the sample: a series of loops that only
 /// touch at perpendicular crossings, with a clear start and end.
-/// Thus, finding the full path is trivial: go forward if possible,
-/// left if not, right if not, and stop when back is the only option.
+/// Thus, finding the full path is trivial: try forward, then left,
+/// then right, and if none of those work then we must be done.
 fn find_full_path(map: &Map) -> Vec<String> {
   let Map {
     scaffolds,
@@ -148,7 +117,7 @@ fn find_full_path(map: &Map) -> Vec<String> {
   let mut bot_y = *bot_y;
   let mut bot_dir = *bot_dir;
 
-  let mut path = vec![];
+  let mut path: Vec<String> = vec![];
   let mut streak = 0;
 
   loop {
@@ -167,7 +136,9 @@ fn find_full_path(map: &Map) -> Vec<String> {
       (bot_x, bot_y) = l_coord;
       bot_dir = bot_dir + Rotation::L;
       if streak > 0 {
-        path.push((streak + 1).to_string());
+        let l = path.len();
+        path[l - 1].extend((streak + 1).to_string().chars());
+        // path.push((streak + 1).to_string());
         streak = 0;
       }
       path.push("L".to_owned());
@@ -175,7 +146,9 @@ fn find_full_path(map: &Map) -> Vec<String> {
       (bot_x, bot_y) = r_coord;
       bot_dir = bot_dir + Rotation::R;
       if streak > 0 {
-        path.push((streak + 1).to_string());
+        let l = path.len();
+        path[l - 1].extend((streak + 1).to_string().chars());
+        // path.push((streak + 1).to_string());
         streak = 0;
       }
       path.push("R".to_owned());
@@ -185,37 +158,144 @@ fn find_full_path(map: &Map) -> Vec<String> {
     }
   }
   if streak > 0 {
-    path.push((streak + 1).to_string());
+    let l = path.len();
+    path[l - 1].extend((streak + 1).to_string().chars());
+    // path.push((streak + 1).to_string());
   }
-
-  // TODO: Okay, this is correct now and so is to_inputs.
-  // The only remaining task is compress_path (the actual hard part).
-  println!("The grid looks like this:");
-  print_grid(&map);
-  println!("\nA full path would look like this:");
-  println!("{path:?}");
-  println!("\nWhich looks like this to the intcode:");
-  println!("{:?}", to_inputs(&path));
 
   path
 }
 
-fn compress_path(full_path: &Vec<String>) -> (Vec<String>, [Vec<String>; 3]) {
-  // TODO: We're looking for three sub-patterns that, when repeated in any permutation,
-  // can reconstruct the original sequence (A,B,C,A,B,B,C for example).
-  // Each sub-pattern, when turned into ASCII via to_input, needs to be 20 bytes max.
-  // Same for the main routine that references them.
-
-  // This looks an awful lot like a Huffman code to me, or perhaps something simpler.
-  // Look into primitive-by-modern-standards lossless compression techniques and see
-  // if one of them matches what we're doing here.
-  // This puzzle smells similar to something telephony companies would have had to
-  // do in the 80's with the hardware of the time, maybe start there?
-  (vec![], [vec![], vec![], vec![]])
+/// Each of (main, A, B, C) must be at most 20 bytes when input, per puzzle description
+fn too_long_path(slice: &Vec<&str>) -> bool {
+  to_inputs(slice).len() > 20
 }
 
-fn to_inputs(from: &Vec<String>) -> Vec<i64> {
+/// The only hard part of the puzzle. It's really just BFS, but with tricky
+/// logic for computing valid neighbors and verifying goal state.
+fn compress_path(full_path: &Vec<String>) -> Result<(Vec<&str>, [Vec<&str>; 3])> {
+  // Use BFS to try patterns until we find one that works
+  let mut frontier: VecDeque<(Vec<&str>, [Option<Vec<&str>>; 3])> = VecDeque::new();
+  frontier.push_back((vec![], [None, None, None]));
+
+  'bfs: while let Some((main_routine, [a_opt, b_opt, c_opt])) = frontier.pop_front() {
+    // Start with only the path that hasn't been consumed yet
+    let mut remain: Vec<&str> = full_path.iter().map(|s| s.as_str()).collect();
+    for &r in main_routine.iter() {
+      if r == "A"
+        && let Some(pat_a) = &a_opt
+      {
+        remain.drain(0..pat_a.len());
+      } else if r == "B"
+        && let Some(pat_b) = &b_opt
+      {
+        remain.drain(0..pat_b.len());
+      } else if r == "C"
+        && let Some(pat_c) = &c_opt
+      {
+        remain.drain(0..pat_c.len());
+      } else {
+        bail!("main_routine had {r} but not a pattern for it");
+      }
+    }
+
+    // If we have a solution at this point, stop
+    if remain.is_empty() {
+      if !too_long_path(&main_routine)
+        && let Some(pat_a) = a_opt
+        && let Some(pat_b) = b_opt
+        && let Some(pat_c) = c_opt
+      {
+        return Ok((main_routine, [pat_a, pat_b, pat_c]));
+      } else if too_long_path(&main_routine) {
+        // This solution can't work, try pursuing others
+        continue 'bfs;
+      } else {
+        bail!(
+          "Entire path was consumed but did not set all three subroutines. This should not be possible."
+        );
+      }
+    }
+
+    // If there's more to consume, search those spaces before we continue
+    let mut explore_more_first = false;
+    if let Some(pat_a) = &a_opt
+      && remain.starts_with(pat_a)
+    {
+      explore_more_first = true;
+      let next_main = [&main_routine[..], &["A"]].concat();
+      frontier.push_back((next_main, [a_opt.clone(), b_opt.clone(), c_opt.clone()]));
+    }
+    if let Some(pat_b) = &b_opt
+      && remain.starts_with(pat_b)
+    {
+      explore_more_first = true;
+      let next_main = [&main_routine[..], &["B"]].concat();
+      frontier.push_back((next_main, [a_opt.clone(), b_opt.clone(), c_opt.clone()]));
+    }
+    if let Some(pat_c) = &c_opt
+      && remain.starts_with(pat_c)
+    {
+      explore_more_first = true;
+      let next_main = [&main_routine[..], &["C"]].concat();
+      frontier.push_back((next_main, [a_opt.clone(), b_opt.clone(), c_opt.clone()]));
+    }
+
+    if explore_more_first {
+      continue 'bfs;
+    }
+
+    // If we got here, we need to discover more paths to try.
+    for i in 1..remain.len() {
+      let mut next_main = main_routine.clone();
+      let mut next_a = a_opt.clone();
+      let mut next_b = b_opt.clone();
+      let mut next_c = c_opt.clone();
+      let candidate: Vec<&str> = remain[0..i].iter().map(|&s| s).collect();
+      if too_long_path(&candidate) {
+        break;
+      }
+      if let Some(_) = a_opt {
+        if let Some(_) = b_opt {
+          if let Some(_) = c_opt {
+            // This solution doesn't consume the whole string, skip it
+            continue 'bfs;
+          } else {
+            next_main.push("C");
+            next_c = Some(candidate);
+          }
+        } else {
+          next_main.push("B");
+          next_b = Some(candidate);
+        }
+      } else {
+        next_main.push("A");
+        next_a = Some(candidate);
+      }
+      frontier.push_back((next_main, [next_a, next_b, next_c]));
+    }
+  }
+
+  bail!("Failed to find a solution");
+}
+
+/// We have either the main routine ["A","B","C",..], or one of the
+/// subroutines like ["L1","R2","L10",..]. Break the latter into a
+/// form like ["L","1","R","2","L","10",..], leave the former alone.
+/// Then convert to their ASCII representations separated by commas.
+fn to_inputs(from: &Vec<&str>) -> Vec<i64> {
   from
+    .iter()
+    .map(|&part| {
+      if part.starts_with("L") || part.starts_with("R") {
+        let (d, c) = part.split_at(1);
+        vec![d, c]
+      } else {
+        vec![part]
+      }
+    })
+    .collect::<Vec<Vec<&str>>>()
+    .concat()
     .join(",")
     .chars()
     .map(|c| c as u8)
@@ -248,16 +328,17 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     Ok(score)
   }
 
-  /// Part 2 is several easy steps, with one hard step being the real puzzle:
-  /// - (Easy) Build the map, which we verified in part 1
-  /// - (Easy) Find the route to touch every point at least once
-  /// - (Hard) Split that into three subroutines that can reconstruct the full path
-  /// - (Easy) Transform that into inputs to the intcode program
-  /// - (Easy) Run the intcode program and return its output directly
+  /// Part 2 is several steps:
+  /// - Build the map, which we verified in part 1
+  /// - Find the route to touch every point at least once
+  /// - Split that into three subroutines that can reconstruct the full path
+  ///   This is the only tricky part, but is ultimately a messy BFS.
+  /// - Transform that into inputs to the intcode program
+  /// - Run the intcode program and return its output directly
   fn part2(&self, instructions: &Parsed, _: Option<String>) -> Result<P2Out> {
     let map = build_map(instructions)?;
     let full_path = find_full_path(&map);
-    let (main_routine, [sub_a, sub_b, sub_c]) = compress_path(&full_path);
+    let (main_routine, [sub_a, sub_b, sub_c]) = compress_path(&full_path)?;
 
     let main_inputs = to_inputs(&main_routine);
     let sub_a_inputs = to_inputs(&sub_a);
@@ -273,7 +354,11 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
       &[10],
       &sub_c_inputs[..],
       &[10],
-      &[110], // Lowercase 'n', for "no I do not want video output"
+      // Lowercase 'n', for "no I do not want video output".
+      // It gives one frame of it at the start anyway though.
+      // It also gives a second frame if the inputs do something wrong.
+      &[110],
+      &[10],
     ]
     .concat();
 
@@ -281,7 +366,9 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     real_instructions[0] = 2;
     let result = execute(&real_instructions, &input, None, None)?;
 
-    Ok(result.outputs[0])
+    // The program outputs text to query for stuff, then outputs the final video feed
+    // even if 'n' was specified, THEN outputs the answer... so get the final number.
+    Ok(*result.outputs.last().unwrap())
   }
 }
 
