b55e33e2e3a7a7972b61580b52fa0e1dd5204fc6
Nick Lanam <nick@nicklanam.com>
2026-08-08 12:05:27 -0700

    2019 Day 23 Part 2

--- 2019/src/bin/day23.rs
+++ 2019/src/bin/day23.rs
@@ -26,8 +26,8 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     loop {
       // Capture output packets first.
       // If any of them are for address 255, return that payload immediately.
-      // TODO: What happens if two executors do that in the same cycle? We'd need to know which one
-      //   did it "first", and the puzzle does not tell us how to decide what "first" means here.
+      // NOTE: There's no case in my input where two executors do this in the same cycle,
+      // so we don't need to figure out which one is "first" here. The puzzle doesn't specify anyway.
       let mut pending_inputs: Vec<Vec<i64>> = (0..50).map(|_| vec![]).collect();
       for (addr, result) in executors.iter().enumerate() {
         if let Ok(execution) = result {
@@ -70,8 +70,78 @@ impl Day<Parsed, P1Out, P2Out> for Solver {
     }
   }
 
-  fn part2(&self, _instructions: &Parsed, _: Option<String>) -> Result<P2Out> {
-    Ok(0)
+  fn part2(&self, instructions: &Parsed, _: Option<String>) -> Result<P2Out> {
+    let mut executors: Vec<Result<Execution>> = (0..50)
+      .map(|n| execute(instructions, &[n], Some(0), None))
+      .collect();
+
+    // Very similar to part 1, except the stop condition is no longer "packet sent to 255".
+    // We watch for those packets, and remember the most recent, then emit that to addr 0
+    // whenever a cycle goes by where all executors are awaiting input but none sent any.
+    // We track what we send to address 0 this way; the first time we see the same Y value
+    // twice, that's our answer.
+    let mut latest_nat: Option<i64> = None;
+
+    loop {
+      let mut nat_packet: Option<(i64, i64)> = None;
+
+      let mut any_pending = false;
+      let mut pending_inputs: Vec<Vec<i64>> = (0..50).map(|_| vec![]).collect();
+      for (addr, result) in executors.iter().enumerate() {
+        if let Ok(execution) = result {
+          for output in execution.outputs.chunks_exact(3) {
+            let [dst, x, y] = *output else {
+              return Err(anyhow!("chunks_exact broke"));
+            };
+            if dst == 255 {
+              nat_packet = Some((x, y));
+              continue;
+            }
+            any_pending = true;
+            pending_inputs[dst as usize].push(x);
+            pending_inputs[dst as usize].push(y);
+          }
+        } else if let Err(err) = result {
+          // Throw the error up, wrapped in the target result type
+          return Err(anyhow!(
+            format!("Executor {} threw an error", addr) + err.to_string().as_str()
+          ));
+        }
+      }
+
+      // Idle: push the stored packet to address 0.
+      // If the most recent such packet we sent had the same Y value, we're done.
+      if !any_pending && let Some(nat_packet) = nat_packet {
+        if let Some(latest_nat) = latest_nat
+          && nat_packet.1 == latest_nat
+        {
+          return Ok(nat_packet.1 as usize);
+        } else {
+          latest_nat = Some(nat_packet.1);
+        }
+        pending_inputs[0].push(nat_packet.0);
+        pending_inputs[0].push(nat_packet.1);
+      }
+
+      // For any executors who are waiting for input, send those packets (or [-1] if nothing is queued)
+      let mut next_executors = vec![];
+      for (addr, result) in executors.iter().enumerate() {
+        if let Ok(result) = result {
+          let inputs = if pending_inputs[addr].is_empty() {
+            vec![-1]
+          } else {
+            pending_inputs[addr].clone()
+          };
+          next_executors.push(execute(
+            &result.final_tape,
+            &inputs,
+            Some(result.pc),
+            Some(result.ro),
+          ));
+        }
+      }
+      executors = next_executors;
+    }
   }
 }
 
